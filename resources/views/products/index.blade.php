b04643f2162e1031dd5e1535bdfe89fbd48af254
Teacher <sapdevm@gmail.com>
2024-07-22 19:15:06 +0500

    1.6.1

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,7 +6,7 @@
             @if(isset($brand)) {{ $brand->name }} @endif
             @if(isset($serie)) {{ $serie->name }} @endif
             @if(isset($category)) {{ $category->name }} @endif
-            @if(!(isset($brand) or isset($serie) or isset($category))) Products @endif
+            @if(!(isset($brand) or isset($serie) or isset($category))) Search @endif
         </div>
         <div class="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5 row-cols-xl-6 g-2 g-sm-3 mb-4">
             @forelse($products as $product)
